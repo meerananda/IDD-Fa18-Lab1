827e54f9717c092e72f21e740182d118e5f15d61
meerananda <meerananda91@gmail.com>
2019-09-09 14:21:23 -0400

    Update NewFade.ino

--- code/NewFade.ino
+++ code/NewFade.ino
@@ -8,7 +8,7 @@
 
 int led = 11;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+int fadeAmount = 12;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -18,7 +18,7 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
+  // set the brightness of pin 11:
   analogWrite(led, brightness);
 
   // change the brightness for next time through the loop:
@@ -28,6 +28,6 @@ void loop() {
   if (brightness <= 0 || brightness >= 255) {
     fadeAmount = -fadeAmount;
   }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+  // wait for 100 milliseconds to see the dimming effect
+  delay(100);
 }
